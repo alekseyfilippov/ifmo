cf56fafdc4bf484ded9324d19d6add928ee6f971
alekseyfilippov <smoky_999@mail.ru>
2019-10-17 20:32:32 +0300

    lesson 2 BiggestInRandom completed

--- src/main/java/com/ifmo/lesson2/BiggestInRandom.java
+++ src/main/java/com/ifmo/lesson2/BiggestInRandom.java
@@ -1,5 +1,7 @@
 package com.ifmo.lesson2;
 
+import java.util.Random;
+
 public class BiggestInRandom {
     /*
      Создать программу, выводящую на экран случайно сгенерированное трёхзначное
@@ -17,15 +19,26 @@ public class BiggestInRandom {
     }
 
     public static int threeDigitRandom() {
-        // TODO implement
+        Random rnd = new Random();
 
-        return 0;
+        return 100 + rnd.nextInt(900);
     }
 
     public static String largestDigit(int rnd) {
         // TODO implement
-
+        int a = rnd / 100;
+        int b = (rnd - a *100) / 10;
+        int c = rnd % 10;
         int max = 0;
+
+        if (a > b && b > c) {
+            max = a;
+        } else if (b > a && b > c) {
+            max = b;
+        } else if (c > a && c > b) {
+            max = c;
+        }
+
 
         return "В числе " + rnd + " наибольшая цифра " + max + ".";
     }
