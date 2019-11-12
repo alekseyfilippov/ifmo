67954e19db2d5aac2699c7d4408fa7e987ec4bac
smoky_999@mail.ru <smok724379>
2019-11-12 17:21:44 +0300

    homework 3 Fibo20 fixed

--- src/main/java/com/ifmo/lesson3/Fibonacci20.java
+++ src/main/java/com/ifmo/lesson3/Fibonacci20.java
@@ -1,5 +1,8 @@
 package com.ifmo.lesson3;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Fibonacci20 {
     /*
     Создайте массив из 20-ти первых чисел Фибоначчи и выведите его на экран. Напоминаем,
@@ -10,12 +13,20 @@ public class Fibonacci20 {
         int[] fibonacciNumbers = fibonacciNumbers();
 
         // TODO implement
+        for (int i : fibonacciNumbers) {
+            System.out.print(i);
+        }
     }
 
     public static int[] fibonacciNumbers() {
         // TODO implement
-
-        return new int[0];
+        int[] fibonachi=new int[20];
+        fibonachi[0]=1;
+        fibonachi[1]=1;
+        for(int i =2; i<=19; i++){
+            fibonachi[i]=fibonachi[i-2]+fibonachi[i-1];
+        }
+        return fibonachi;
     }
 
 }
