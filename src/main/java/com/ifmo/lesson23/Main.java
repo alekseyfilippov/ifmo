31b7c54f7c76031c8faa813562c7d8ecda1294ea
alekseyfilippov <smoky_999@mail.ru>
2019-12-08 12:42:28 +0300

    Lesson 24 Bank complete

--- src/main/java/com/ifmo/lesson23/Main.java
+++ src/main/java/com/ifmo/lesson23/Main.java
@@ -1,5 +1,56 @@
 package com.ifmo.lesson23;
 
-public class Main {
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        int cpus = Runtime.getRuntime().availableProcessors();
+        // Создём пул потоков.
+        ExecutorService pool = Executors.newFixedThreadPool(cpus);
+
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>(cpus);
+        // Отправляем задачи на обработку.
+        for (int i = 0; i < cpus; i++) {
+            List<String> lines = List.of(); // todo 1/cpus строк
+
+            Future<Map<String, Integer>> future =
+                    pool.submit(() -> countWords(lines));
+
+            futures.add(future);
+        }
+
+        List<Map<String, Integer>> counts = new ArrayList<>(cpus);
+        // Собираем результаты выполнения.
+        for (Future<Map<String, Integer>> future : futures) {
+            Map<String, Integer> map = future.get();
+
+            counts.add(map);
+        }
+        // Объединяем все счётчики и находим 10 наиболее часто
+        // встречающихся слов.
+        List<String> top10Words = top10(counts);
+
+        System.out.println(top10Words);
+
+        pool.shutdown();
+    }
+
+    private static Map<String, Integer> countWords(List<String> lines) {
+        // todo implement
+
+        return Map.of();
+    }
+
+    private static List<String> top10(Collection<Map<String, Integer>> counters) {
+        // todo implement
+
+        return List.of();
+    }
 }
+
--- src/main/java/com/ifmo/lesson24/Bank.java
+++ src/main/java/com/ifmo/lesson24/Bank.java
@@ -1,14 +1,17 @@
 package com.ifmo.lesson24;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Random;
+import java.util.concurrent.*;
 
 public class Bank {
     private Map<Long, User> users = new ConcurrentHashMap<>();
     private List<Account> accounts = new CopyOnWriteArrayList<>();
+    private LinkedBlockingQueue<Transaction> linkedBlockingQueue = new LinkedBlockingQueue<>();
+
 
 
     private class User {
@@ -70,7 +73,40 @@ public class Bank {
         // Другими словами, создайте 100 потоков или пул из 100 потоков, в которых
         // выполните перевод вызовом метода transferMoney().
 
+        Bank bank = new Bank();
 
+        List<User> users = new ArrayList<>(40);
+        List<Account> accounts = new ArrayList<>(40);
+
+        for (int i = 0; i < 20; i++) {
+            users.add(bank.new User(i, "Name" + i));
+            accounts.add(bank.new Account(i, i, new Random(System.currentTimeMillis()).nextInt(100000)));
+        }
+
+        Thread logger = new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println(bank.linkedBlockingQueue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        logger.start();
+        ExecutorService pool = Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 100; i++) {
+            pool.submit(() -> {
+                bank.transferMoney(accounts.get(new Random().nextInt(accounts.size())), accounts.get(new Random().nextInt(accounts.size())), new Random(System.currentTimeMillis()).nextInt(100000));
+            });
+        }
+        pool.shutdown();
+
+        try {
+            logger.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -79,5 +115,32 @@ public class Bank {
         // 1. Атомарно и потокобезопасно перевести деньги в количестве 'amount' со счёта 'from' на счёт 'to'.
         // 2. Создать объект Transaction, содержащий информацию об операции и отправить в очередь
         // потоку Logger, который проснётся и напечатает её.
+
+        //same account
+        if (from.id == to.id)
+            return;
+        //not enouth money
+        if (from.amount < amount) {
+            try {
+                linkedBlockingQueue.put(new Transaction(from.id, to.id, amount, false));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+
+        synchronized (from) {
+            synchronized (to) {
+                from.amount -= amount;
+                to.amount += amount;
+            }
+        }
+        try {
+            linkedBlockingQueue.put(new Transaction(from.id, to.id, amount, true));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
